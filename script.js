8f31257414567b17bb1e44bb9db0bde64eb96981
Raul-Araujo <raul31_lpcn@hotmail.com>
2020-08-30 23:51:19 -0300

    finished the function that creates the squares

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const container  = document.querySelector('#container');
 
-function creatingSquares(columns){
-    for (let i = 0; i < columns; i++){
-        
-        const squares = document.createElement('div');
-        squares.setAttribute('style', 'border: 1px black solid; height:16px; width:16px;');
-        container.appendChild(squares);
-        
-    }    
-}
-creatingSquares(10);
+    function creatingSquares(columns){
+
+     for (let i = 0; i < columns; i++){
+            const squares = document.createElement('div');
+            squares.classList.add('columns'+[i]);
+            squares.setAttribute('style', 'background: white;');
+            container.appendChild(squares);
+
+                for (let x = 0; x < columns; x++){
+                    const columns1 = document.querySelector('.columns'+[i]);
+                    const squares1 = document.createElement('div');
+                    squares1.classList.add(`columns1`+[x]);
+                    squares1.setAttribute('style', 'background: white');
+                    columns1.appendChild(squares1);
+                    
+                }
+        }   
+    }
+creatingSquares(10); //The above function creates the square.
